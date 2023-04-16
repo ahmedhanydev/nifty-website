f53e451f3392955a189bd792366c7f49daaf28a4
ahmedhany <elgenius10000@gmail.com>
2023-04-16 05:13:17 +0200

    change in projecty

--- src/App.js
+++ src/App.js
@@ -7,7 +7,26 @@ import Mint from "./Pages/Mint/Mint";
 import Header from "./Components/Header/Header";
 // import Menu from "./Components/Menu/Menu";
 import Footer from "./Components/Footer/Footer";
+import { useEffect } from "react";
 const App = () => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.keyCode === 123) {
+        // F12 key
+        event.preventDefault();
+      }
+    };
+    const handleContextMenu = (event) => {
+      event.preventDefault();
+    };
+    window.addEventListener("contextmenu", handleContextMenu);
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
   return (
     <DataContextProvider>
       <Header />
